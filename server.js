d921159fa565b376d97176089c02217d945b80d4
Mike Brevoort <mike@brevoort.com>
2017-01-10 21:14:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
 var server = slapp.attachToExpress(express())
 
 server.get('/health', (req, res) => {
-  res.send('ok 15')
+  res.send('ok 16')
 })
 
 // start http server
